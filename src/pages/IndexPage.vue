cd557e7e0f372abcd7a530dc43b4df72c3af6f32
BobaRoltan <Rost.kuksa@gmail.com>
2025-04-15 18:01:17 +0300

    Делаю проект.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,24 +2,30 @@
 import {ref} from "vue"
 let text = ref('')
 let text1 = ref('')
+let tochki = ref([])
 function proverka(){
   if(text.value===''){
     alert('Напишите длину рельса')
   }
-  if (isNaN(text.value) === true) {
-    alert('Напишите длину рельса числом')
+  if (isNaN(text.value) === true || Number.isInteger(+text.value) === false) {
+    alert('Напишите длину рельса целым числом')
   }
+  console.log(Number.isInteger(+text.value))
   if (text.value < 0) {
-    alert('Напишите длину рельса правильно')
+    alert('Напишите длину рельса положительным числом')
   }
   if (text1.value===''){
     alert('Напишите координат дефекта')
   }
   if (isNaN(text1.value) === true) {
-    alert('Напишите координат дефекта числом')
+    alert('Напишите координат дефекта целым числом')
   }
   if (text1.value < 0) {
-    alert('Напишите координат дефекта правильно')
+    alert('Напишите координат дефекта положительным числом')
+  }
+  if (text1.value > 10) {
+    let tochka = text.value / 10
+    tochki.value = tochka
   }
 }
 </script>
@@ -33,7 +39,21 @@ function proverka(){
     Напишите координат дефекта: <q-input v-model="text1" class="inp" />см
   </div>
   <q-btn color="orange" label="Сканирование" @click="proverka"></q-btn>
+  <div class="radar"></div>
+  <div class="relsa"></div>
 </template>
 
 <style>
+.relsa{
+  background-color: #1D1D1D;
+  margin-top: 50px;
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+.radar{
+  background-color: black;
+  padding-bottom: 5px;
+  padding-top: 5px;
+  border-right: 100px;
+}
 </style>
