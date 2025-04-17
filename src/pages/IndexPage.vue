10c04cd9c6f8bd4d2ff740cbc07bf7bb83021234
BobaRoltan <Rost.kuksa@gmail.com>
2025-04-17 18:00:25 +0300

    Делаю проект.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,6 +3,8 @@ import {ref} from "vue"
 let text = ref('')
 let text1 = ref('')
 let tochki = ref([])
+let radar = ref(0)
+
 function proverka(){
   if(text.value===''){
     alert('Напишите длину рельса')
@@ -10,23 +12,34 @@ function proverka(){
   if (isNaN(text.value) === true || Number.isInteger(+text.value) === false) {
     alert('Напишите длину рельса целым числом')
   }
-  console.log(Number.isInteger(+text.value))
   if (text.value < 0) {
     alert('Напишите длину рельса положительным числом')
   }
   if (text1.value===''){
     alert('Напишите координат дефекта')
   }
-  if (isNaN(text1.value) === true) {
-    alert('Напишите координат дефекта целым числом')
+  //if (isNaN(text1.value) === true) {
+  //  alert('Напишите координат дефекта целым числом')
+  // }
+  // if (text1.value < 0) {
+  //   alert('Напишите координат дефекта положительным числом')
+  // }
+  // if (text1.value > 10) {
+  //   let tochka = text.value / 10
+  //   tochki.value = tochka
+  // }
+  tochki.value = text1.value.split(', ')
+  let newTochki = []
+  for (let t of tochki.value) {
+    let x = (1920 * t) / text.value
+    newTochki.push(x)
   }
-  if (text1.value < 0) {
-    alert('Напишите координат дефекта положительным числом')
-  }
-  if (text1.value > 10) {
-    let tochka = text.value / 10
-    tochki.value = tochka
-  }
+  tochki.value = newTochki
+
+  let interval = setInterval(() => {
+    radar.value = radar.value + (1920 * 10) / text.value
+    console.log(radar.value)
+  }, 1000)
 }
 </script>
 
@@ -39,21 +52,37 @@ function proverka(){
     Напишите координат дефекта: <q-input v-model="text1" class="inp" />см
   </div>
   <q-btn color="orange" label="Сканирование" @click="proverka"></q-btn>
-  <div class="radar"></div>
-  <div class="relsa"></div>
+  <div class="relsa">
+    <div class="radar"
+         :style="{ 'left': `${radar}px`}"
+    ></div>
+    <div
+      v-for="defect in tochki"
+      :style="{ 'left': `${ defect }px`}"
+      class="defectx"
+    ></div>
+  </div>
 </template>
 
 <style>
-.relsa{
+.relsa {
+  position: relative;
   background-color: #1D1D1D;
-  margin-top: 50px;
-  padding-bottom: 10px;
-  padding-top: 10px;
+  width: 100%;
+  height: 50px;
 }
 .radar{
-  background-color: black;
-  padding-bottom: 5px;
-  padding-top: 5px;
-  border-right: 100px;
+  position: absolute;
+  left: 0;
+  background-color: aliceblue;
+  width: 10px;
+  height: 100%;
+}
+.defectx{
+  position: absolute;
+  left: 0;
+  width: 16px;
+  height: 16px;
+  background-color: red;
 }
 </style>
